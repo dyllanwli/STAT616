065624d0d93d789898e528b27cb41ce952780e9f
dyllanwli <messsking@gmail.com>
2020-11-19 19:20:54 -0600

    update readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 + https://github.com/YEN-GitHub/GradientDescent_OptimizationAlgorithms
 + https://github.com/harshraj11584/Paper-Implementation-Overview-Gradient-Descent-Optimization-Sebastian-Ruder
 + https://towardsdatascience.com/implementation-of-optimization-for-deep-learning-highlights-in-2017-feat-sebastian-ruder-616766fe37f0
+
++ https://github.com/nrsharip/gradient-descent/blob/master/jupyter/workspace.ipynb
---
+++ utils.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+def read_data():
+    p = "./data/normalized.csv"
+    df = pd.read_csv(p)
+    return df['RM'], df['PRICE']
+
+def da(y,y_p,x):
+    return (y-y_p)*(-x)
+
+def db(y,y_p):
+    return (y-y_p)*(-1)
+
+def calc_loss(a,b,x,y):
+    tmp = y - (a * x + b)
+    tmp = tmp ** 2  # 对矩阵内的每一个元素平方
+    SSE = sum(tmp) / (2 * len(x))
+    return SSE
+
+def draw_hill(x, y):
+    a = np.linspace(-20, 20, 100)
+    print(a)
+    b = np.linspace(-20, 20, 100)
+    x = np.array(x)
+    y = np.array(y)
+
+    allSSE = np.zeros(shape=(len(a),len(b)))
+    for ai in range(0,len(a)):
+        for bi in range(0,len(b)):
+            a0 = a[ai]
+            b0 = b[bi]
+            SSE = calc_loss(a=a0, b=b0, x=x, y=y)
+            allSSE[ai][bi] = SSE
+
+    a,b = np.meshgrid(a, b)
+
+    return [a, b, allSSE]
